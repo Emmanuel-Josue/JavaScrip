5c33e5bfb2059e0e683844258fb1dde5742eca68
Emmanuel <jos.emma3214@gmail.com>
2023-05-25 00:01:50 -0600

    Se toma una clase

---
+++ cursoYouTube/operadoresLogicos.js
@@ -0,0 +1,25 @@
+/*
+    Con los operadores logicos podremos utilizar multiples sentencias, existen basicamente tres tipo de operadores 
+
+    && ( Y )
+    true && true = true
+    true && false = false
+    false && true = false
+    false && false = false
+
+    -------------------------
+
+    || ( o )
+    true || false = true
+    false || true = true 
+    true || true = true 
+    false || false = false 
+
+    ------------------------------------------
+
+    ! NOT (Negacion o inverciön)
+    ! (true) = false
+    ! (false) = true
+
+
+ */
