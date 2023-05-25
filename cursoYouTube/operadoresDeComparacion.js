97f55c552c2c10808da417fe772cc75733e355ee
Emmanuel <jos.emma3214@gmail.com>
2023-05-24 21:16:18 -0600

    Se toman dos clases, operadores de comparación y sobre verdades

---
+++ cursoYouTube/operadoresDeComparacion.js
@@ -0,0 +1,15 @@
+// > Mayor que 
+// < Menor que 
+
+// >= Mayor o igual que 
+// <= Menor o igual que 
+
+// == igual que (Solo compara el valor)
+// != NO es igual que (Solo compara valor)
+
+// === Completamete igual que (Comapra valor y tipo de dato )
+// !=== NO es igual que o es diferente que (compara valor y tipo de dato)
+
+// Se recomienda siempre utilizar los operadores que comparen el valor y el tipo de dato 
+
+// Anidar en programación es colocar un código adentro de otro 
---
+++ cursoYouTube/verdades.js
@@ -0,0 +1,4 @@
+// Por defecto todos los números dan como resultado true excepto el cero ya que este da como resultado false, también 
+// las dobles commiyas sin nada en su interior, nisiquiera un espacio, da false, una sola comilla también da falso, null 
+// también es falso, el undefine también es falso, NaN también es falso 
+// 0/0 también da falso 
